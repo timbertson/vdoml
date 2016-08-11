2e23eef20359c73136f937cfe4be18223e475e8d
Tim Cuthbertson <tim@gfxmonk.net>
2016-08-11 22:31:59 +1000

    extract collection_cache

---
+++ src/collection_cache.ml
@@ -0,0 +1,45 @@
+module type COLLECTION = sig
+  type ('k, 'v) t
+  val empty: ('k, 'v) t
+  val add : 'k -> 'v -> ('k, 'v) t -> ('k, 'v) t
+  val find : 'k -> ('k, 'v) t -> 'v option
+end
+
+module Child_list : COLLECTION = struct
+  type ('k, 'v) t = ('k * 'v) list
+  let empty = []
+  let add key value lst = (key,value)::lst
+  let find key lst =
+    try
+      Some (match List.find
+        (fun (candidate_key, _) -> key = candidate_key) lst
+      with (key,value) -> value)
+    with Not_found -> None
+end
+
+module Make(Collection:COLLECTION) = struct
+  type ('k, 'v) state = {
+    active : ('k, 'v) Collection.t ref;
+    next : ('k, 'v) Collection.t ref;
+  }
+
+  let init () = {
+    active = ref Collection.empty;
+    next = ref Collection.empty;
+  }
+
+  let use state =
+      let get id fn =
+        let found = match (Collection.find id !(state.active)) with
+          | None -> fn ()
+          | Some cached -> cached
+        in
+        state.next := Collection.add id found !(state.next);
+        found
+      in
+    fun fn ->
+      state.next := Collection.empty;
+      let rv = fn get in
+      state.active := !(state.next);
+      rv
+end
--- src/ui.ml
+++ src/ui.ml
@@ -142,54 +142,8 @@ let render
 
 let emit instance = instance.Component.emit
 
-module type COLLECTION = sig
-  type ('k, 'v) t
-  val empty: ('k, 'v) t
-  val add : 'k -> 'v -> ('k, 'v) t -> ('k, 'v) t
-  val find : 'k -> ('k, 'v) t -> 'v option
-end
-
-module ChildList : COLLECTION = struct
-  type ('k, 'v) t = ('k * 'v) list
-  let empty = []
-  let add key value lst = (key,value)::lst
-  let find key lst =
-    try
-      Some (match List.find
-        (fun (candidate_key, _) -> key = candidate_key) lst
-      with (key,value) -> value)
-    with Not_found -> None
-end
-
-module CollectionCache(Collection:COLLECTION) = struct
-  type ('k, 'v) state = {
-    active : ('k, 'v) Collection.t ref;
-    next : ('k, 'v) Collection.t ref;
-  }
-
-  let init () = {
-    active = ref Collection.empty;
-    next = ref Collection.empty;
-  }
-
-  let use state =
-      let get id fn =
-        let found = match (Collection.find id !(state.active)) with
-          | None -> fn ()
-          | Some cached -> cached
-        in
-        state.next := Collection.add id found !(state.next);
-        found
-      in
-    fun fn ->
-      state.next := Collection.empty;
-      let rv = fn get in
-      state.active := !(state.next);
-      rv
-end
-
 (* default implementation: just use a list *)
-module ChildCache = CollectionCache(ChildList)
+module ChildCache = Collection_cache.Make(Collection_cache.Child_list)
 
 let children ~view ?message ~id instance =
   let open Vdom.Pure in
