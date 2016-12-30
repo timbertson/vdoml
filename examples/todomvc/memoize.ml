bc88c394b57a28c67206cd698de35e747ebd829d
Tim Cuthbertson <tim@gfxmonk.net>
2016-12-30 22:45:19 +1100

    remove curry module

--- examples/todomvc/memoize.ml
+++ examples/todomvc/memoize.ml
@@ -1,5 +1,3 @@
-open Log_
-
 type ('arguments, 'ret) cached_function = 'arguments -> 'ret
 type ('arguments, 'ret) cache = {
   fn: ('arguments -> 'ret);
@@ -10,10 +8,8 @@ let init fn =
   fun args ->
     match !(cache.current) with
       | Some (old_args, old_ret) when old_args = args ->
-        Log.debug (fun m->m "curry cache hit");
         old_ret
       | Some _ | None ->
-        Log.debug (fun m->m "curry cache miss");
         let ret = cache.fn args in
         cache.current := Some (args, ret);
         ret
