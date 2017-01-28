be55acacad0b5e3b4af22e0d9afadef8445afbb4
Tim Cuthbertson <tim@gfxmonk.net>
2017-01-28 17:40:03 +1100

    bump opam2nix; massively simplifies default.nix

--- nix/default.nix
+++ nix/default.nix
@@ -3,61 +3,11 @@ with pkgs;
 let
 	chompFile = file: lib.removeSuffix "\n" (builtins.readFile file);
 	opam2nix = callPackage ./opam2nix-packages.nix {};
-	opamDeps = ["ppx_test" "ppx_assert"] ++ (lib.splitString "," (chompFile ./opam-dep-names));
-	opamConfig = {
-		packages = opamDeps;
-		ocamlAttr = "ocaml_4_02";
-		args= ["--verbose" ];
-	};
-	makeOpamRepository = {
-		name, version, opamFile, src,
-		descr ? "anonymous opam package"
-	}:
-		derivation {
-			name = "opam-custom";
-			system = builtins.currentSystem;
-			builder =
-			pkgs.writeScript "opam-builder.sh"
-				''#!${pkgs.bash}/bin/bash
-					export PATH="${pkgs.coreutils}/bin"
-					dest="$out/packages/${name}/${name}.${version}"
-					mkdir -p "$dest"
-					cd "$dest"
-					cp ${opamFile} ./opam
-					cat > ./descr <<'END_DESCR'
-${descr}
-END_DESCR
-					if [ -f "${src}" ]; then
-						echo 'archive: "${src}"' > url
-					else
-						echo 'src: "${src}"' > url
-					fi
-			'';
-		};
 in
-stdenv.mkDerivation {
-	name = "vdoml";
-	passthru = {
-		opamDepNames = opamDeps;
-		opamDeps = opam2nix.buildPackageSet opamConfig;
-		selections = opam2nix.selectionsFileLax opamConfig;
-		opamRepo = makeOpamRepository {
-			name = "vdoml";
-			version = chompFile ../VERSION;
-			opamFile = ../vdoml.opam;
-			inherit src;
-		};
-		inherit (opam2nix) opam2nix;
-	};
+opam2nix.buildOpamPackage {
+	name = "vdoml-${chompFile ../VERSION}";
 	inherit src;
-	buildInputs = (opam2nix.build opamConfig) ++ [python];
-	buildPhase = ''
-		tools/bin/gup lib
-	'';
-	installPhase = ''
-		mkdir -p "$out"
-		./install.sh
-	'';
+	ocamlAttr = "ocaml_4_03";
 	shellHook = ''
 		export OCAMLPATH="$OCAMLPATH:$PWD/lib"
 	'';
--- nix/opam2nix-packages.nix
+++ nix/opam2nix-packages.nix
@@ -5,23 +5,23 @@ let
 	# For development, set OPAM2NIX_DEVEL to your local
 	# opam2nix repo path
 	devRepo = builtins.getEnv "OPAM2NIX_DEVEL";
-	src = fetchgit {
-  "url" = "https://github.com/timbertson/opam2nix-packages.git";
-  "fetchSubmodules" = false;
-  "sha256" = "0haxgzwzhb2dcs1rlbdywrgxbldrwr9glgv4czap23d43wyclyfp";
-  "rev" = "eaff5549aa9c3a154caad8e64e90c8f653ccbf78";
-};
-	opam2nix = fetchgit {
-  "url" = "https://github.com/timbertson/opam2nix.git";
-  "fetchSubmodules" = false;
-  "sha256" = "0w13aabqwqvkrxvd98c2fpfran7si2rmqjym0lpdwns8mwghdw1v";
-  "rev" = "aaa902104e1a98b6d907420549580537d1feee8e";
-};
+	src = fetchgit 	{
+		"url" = "https://github.com/timbertson/opam2nix-packages.git";
+		"fetchSubmodules" = false;
+		"sha256" = "05cblj9lwnwssfpc9xfhmgpipffngkq2mq9q1zgka6523si7ij7g";
+		"rev" = "da773505d596822994b608f444f18157ad0e9a7a";
+	};
+	opam2nix = fetchgit 	{
+		"url" = "https://github.com/timbertson/opam2nix.git";
+		"fetchSubmodules" = false;
+		"sha256" = "0w13aabqwqvkrxvd98c2fpfran7si2rmqjym0lpdwns8mwghdw1v";
+		"rev" = "aaa902104e1a98b6d907420549580537d1feee8e";
+	};
 in
 if devRepo != "" then
 	let toPath = s: /. + s; in
 	callPackage "${devRepo}/nix" {} {
-			src = toPath "${devRepo}/nix/local.tgz";
-			opam2nix = toPath "${devRepo}/opam2nix/nix/local.tgz";
-		}
+		src = toPath "${devRepo}/nix/local.tgz";
+		opam2nix = toPath "${devRepo}/opam2nix/nix/local.tgz";
+	}
 else callPackage "${src}/nix" {} { inherit src opam2nix; }
