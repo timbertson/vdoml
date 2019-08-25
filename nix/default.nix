779bbdbef0aa6869eaa677cf1c8ce3aa2071601d
Tim Cuthbertson <tim@gfxmonk.net>
2019-08-25 21:32:39 +1000

    fix `src`

--- nix/default.nix
+++ nix/default.nix
@@ -1,11 +1,12 @@
-{ pkgs ? import <nixpkgs> {}, opam2nix, ocamlAttr ? "ocaml-ng.ocamlPackages_4_06.ocaml" }:
+{ pkgs ? import <nixpkgs> {}, opam2nix, ocamlAttr ? "ocaml-ng.ocamlPackages_4_06.ocaml", self ? ../. }:
 with pkgs;
 let
 	chompFile = file: lib.removeSuffix "\n" (builtins.readFile file);
 in
 opam2nix.buildOpamPackage {
 	name = "vdoml-${chompFile ../VERSION}";
-	inherit src ocamlAttr;
+	src = self;
+	inherit ocamlAttr;
 	opamFile = ../vdoml.opam;
 	extraPackages = [ "ppx_inline_test" "sexplib" "ppx_assert" ];
 	shellHook = ''
