2cb00548ecbb512ec22865fcc8b7e0c831dc8a60
Tim Cuthbertson <tim@gfxmonk.net>
2019-08-25 13:48:37 +1000

    use nix-wrangle

--- nix/default.nix
+++ nix/default.nix
@@ -1,12 +1,7 @@
-{ pkgs ? import <nixpkgs> {}, opam2nix ? pkgs.callPackage ./opam2nix-packages {}, ocamlAttr ? "ocaml-ng.ocamlPackages_4_06.ocaml" }:
+{ pkgs ? import <nixpkgs> {}, opam2nix, ocamlAttr ? "ocaml-ng.ocamlPackages_4_06.ocaml" }:
 with pkgs;
 let
 	chompFile = file: lib.removeSuffix "\n" (builtins.readFile file);
-	opam-installer = callPackage ./opam-installer.nix { inherit opam2nix; };
-	isStorePath = x: lib.isStorePath (builtins.toString x); # workaround https://github.com/NixOS/nixpkgs/issues/48743
-	src = if isStorePath ../.
-		then ../.
-		else (nix-update-source.fetch ./src.json).src;
 in
 opam2nix.buildOpamPackage {
 	name = "vdoml-${chompFile ../VERSION}";
