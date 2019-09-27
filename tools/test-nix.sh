1c0cea1df00e6da53efe6d4ae0e101234da45cf5
Tim Cuthbertson <tim@gfxmonk.net>
2019-09-27 16:18:29 +1000

    fix CI

--- tools/test-nix.sh
+++ tools/test-nix.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -eux
 
-NIX_PIN="$(nix-build --no-out-link '<nixpkgs>' -A 'nix-pin')/bin/nix-pin"
 set +x
-"$NIX_PIN" create vdoml . --path nix/default.nix
 bash <(curl -sSL 'https://gist.githubusercontent.com/timbertson/b619802d795c270f80886cee8efd4191/raw/travis-long-output.sh') \
-	"$NIX_PIN" build --show-trace .
+	nix-build --show-trace .
 echo "== Built files:"
 ls -lR result/
 
