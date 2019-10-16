f3b9e7b55e0aef12bb6f848f83aa7bfca5c29b7c
Tim Cuthbertson <tim@gfxmonk.net>
2019-10-16 21:15:59 +1100

    update opam2nix

--- nix/default.nix
+++ nix/default.nix
@@ -2,18 +2,26 @@
 	pkgs ? import <nixpkgs> {},
 	opam2nix,
 	ocamlAttr ? "ocaml-ng.ocamlPackages_4_06.ocaml",
+	nix-wrangle,
+	self,
 }:
 with pkgs;
 let
 	chompFile = file: lib.removeSuffix "\n" (builtins.readFile file);
+	opamPackages = opam2nix.build {
+		src = self;
+		deps = ../opam-packages.nix;
+		ocaml = ocaml-ng.ocamlPackages_4_08.ocaml;
+	};
 in
-opam2nix.buildOpamPackage {
-	name = "vdoml-${chompFile ../VERSION}";
-	src = ../.;
-	inherit ocamlAttr;
-	opamFile = ../vdoml.opam;
-	extraPackages = [ "ppx_inline_test" "sexplib" "ppx_assert" ];
-	shellHook = ''
-		export OCAMLPATH="$OCAMLPATH:$PWD/lib"
-	'';
+{
+	inherit opam2nix;
+	inherit (opamPackages) vdoml;
+	shell = with opamPackages; vdoml.overrideAttrs (o: {
+		buildInputs = (o.buildInputs or [])
+			++ [ ppx_inline_test sexplib ppx_assert ];
+		shellHook = ''
+			export OCAMLPATH="$OCAMLPATH:$PWD/lib"
+		'';
+	});
 }
--- tools/test-nix.sh
+++ tools/test-nix.sh
@@ -3,7 +3,7 @@ set -eux
 
 set +x
 bash <(curl -sSL 'https://gist.githubusercontent.com/timbertson/b619802d795c270f80886cee8efd4191/raw/travis-long-output.sh') \
-	nix-build --show-trace .
+	nix-build --show-trace -A vdoml
 echo "== Built files:"
 ls -lR result/
 
