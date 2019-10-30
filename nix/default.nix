a1ac506ab027105acd1e27dd1d065ad36f7ac5ac
Tim Cuthbertson <tim@gfxmonk.net>
2019-10-31 07:46:05 +1100

    make default expression the derivation

--- nix/default.nix
+++ nix/default.nix
@@ -13,12 +13,13 @@ let
 		selection = ./opam-selection.nix;
 		ocaml = ocaml-ng.ocamlPackages_4_08.ocaml;
 	};
+	result = {
+		inherit opam2nix;
+		inherit (opamPackages) vdoml;
+		shell = with opamPackages; vdoml.overrideAttrs (o: {
+			buildInputs = (o.buildInputs or [])
+				++ [ ppx_inline_test sexplib ppx_assert ];
+		});
+	};
 in
-{
-	inherit opam2nix;
-	inherit (opamPackages) vdoml;
-	shell = with opamPackages; vdoml.overrideAttrs (o: {
-		buildInputs = (o.buildInputs or [])
-			++ [ ppx_inline_test sexplib ppx_assert ];
-	});
-}
+result.vdoml.overrideAttrs (_: { passthru = result; })
